68d45af99d9c961254816e1e6d15d0869bbb7d1f
abhishekazadprince <abhishekazadprince@gmail.com>
2025-11-24 01:58:12 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'physics-app-v7';
+const CACHE_NAME = 'physics-app-v8';
 const urlsToCache = [
   './',
   './index.html',
@@ -37,5 +37,6 @@ self.addEventListener('fetch', event => {
       .then(response => response || fetch(event.request))
   );
 });
+
 
 
