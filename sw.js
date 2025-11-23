1349e24330f81b16ac5d411f4fd0b91e9dabcee7
abhishekazadprince <abhishekazadprince@gmail.com>
2025-11-23 16:15:54 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'kalasimx-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './KalaSimX.jpg'
+];
+
+// Install event - caches the files
+self.addEventListener('install', event => {
+  self.skipWaiting(); // Forces this new worker to activate immediately
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate event - cleans up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim(); // Takes control of the page immediately
+});
+
+// Fetch event - serves from cache or network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
